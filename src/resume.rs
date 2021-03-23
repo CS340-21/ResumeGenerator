c3db6939a6480841446863adc9997104ae4bc57e
Adam McDaniel <adam.mcdaniel17@gmail.com>
2021-03-23 18:36:24 -0400

    Added more themes, theme control over CSS, and more HTML structures

--- src/resume.rs
+++ src/resume.rs
@@ -56,11 +56,11 @@ pub enum Proficiency {
 impl ToString for Proficiency {
     fn to_string(&self) -> String {
         match self {
-            Self::None => String::from("none"),
-            Self::Barely => String::from("barely"),
-            Self::Some => String::from("some"),
-            Self::Strong => String::from("strong"),
-            Self::Expert => String::from("expert"),
+            Self::None => String::from("None"),
+            Self::Barely => String::from("Barely"),
+            Self::Some => String::from("Some"),
+            Self::Strong => String::from("Strong"),
+            Self::Expert => String::from("Expert"),
         }
     }
 }
@@ -78,11 +78,21 @@ impl From<Proficiency> for u32 {
 }
 
 #[derive(Clone, Debug, PartialEq, Eq, PartialOrd, Ord)]
+pub struct ContactInfo {
+    pub email:    Option<String>,
+    pub phone:    Option<String>,
+    pub website:  Option<String>,
+    pub github:   Option<String>,
+    pub linkedin: Option<String>,
+}
+
+#[derive(Clone, Debug, PartialEq, Eq, PartialOrd, Ord)]
 pub struct Resume {
     pub first_name: String,
     pub last_name: String,
 
     pub description: String,
+    pub contact_info: ContactInfo,
 
     pub profession: String,
     pub skills: Vec<(String, Option<Proficiency>)>,
@@ -93,47 +103,56 @@ pub struct Resume {
 impl Resume {
     pub fn generate(&self) -> HTML {
         HTML::html(vec![
-            HTML::row(vec![
-                HTML::section(HTML::col(vec![
-                    HTML::aligned(
-                        HTML::title(format!("{} {}", self.first_name, self.last_name)),
-                        HorizontalAlignment::Center,
-                        VerticalAlignment::SameAsParent
-                    ),
-                    HTML::section_title(&self.profession),
-                    HTML::text(&self.description),
-                ])),
-                HTML::section(HTML::col(vec![
-                    HTML::aligned(
-                        HTML::section_title("Skills"),
-                        HorizontalAlignment::Center,
-                        VerticalAlignment::SameAsParent
-                    ),
-                    HTML::aligned(HTML::ul(self.skills.iter().map(|(skill, level)| {
-                        if let Some(level) = level {
-                            HTML::row(vec![HTML::text(skill), HTML::PercentBar(u32::from(*level) * 100 / u32::from(Proficiency::Expert), level.to_string())])
-                        } else {
-                            HTML::text(skill)
-                        }
-                    }).collect::<Vec<HTML>>()), HorizontalAlignment::Center,
-                    VerticalAlignment::SameAsParent)
-                ])),
-                // HTML::section(HTML::aligned(HTML::text("hello world!"), HorizontalAlignment::Center, VerticalAlignment::Center)),
-            ]),
-            HTML::section(HTML::col(vec![
-                HTML::aligned(HTML::section_title("Education"), HorizontalAlignment::Center, VerticalAlignment::SameAsParent),
-                HTML::ol(self.education.iter().map(|e| {
-                    if let Some(degree) = &e.degree {
-                        HTML::text(format!("Attended <b>{}</b> from <i>{}</i> to <i>{}</i> and acheived {}", e.school, e.start_year, e.end_year, degree.to_string()))
-                    } else {
-                        HTML::text(format!("Attended <b>{}</b> from <i>{}</i> to <i>{}</i>", e.school, e.start_year, e.end_year))
-                    }
-                }).collect::<Vec<HTML>>()),
-                HTML::aligned(HTML::section_title("Work Experience"), HorizontalAlignment::Center, VerticalAlignment::SameAsParent),
-                HTML::ul(self.work_experience.iter().map(|j| {
-                    HTML::text(format!("<b>{}</b> at {} from <i>{}</i> to <i>{}</i>", j.position, j.company, j.start_year, j.end_year))
-                }).collect::<Vec<HTML>>())
-            ])),
+            HTML::container(vec![
+                HTML::col(vec![
+                    HTML::row(vec![
+                        HTML::section(HTML::col(vec![
+                            HTML::aligned(
+                                HTML::fg(HTML::title(format!("{} {}", self.first_name, self.last_name)), Color::Violet),
+                                HorizontalAlignment::Center,
+                                VerticalAlignment::SameAsParent
+                            ),
+                            HTML::aligned(
+                                HTML::fg(HTML::italics(HTML::section_title(&self.profession)), Color::Pink),
+                                HorizontalAlignment::Center,
+                                VerticalAlignment::SameAsParent
+                            ),
+                            // HTML::fg(HTML::text(&self.description), Color::Green),
+                            HTML::text(&self.description),
+                        ])),
+                        HTML::section(HTML::col(vec![
+                            HTML::aligned(
+                                HTML::fg(HTML::italics(HTML::section_title("Skills")), Color::Pink),
+                                HorizontalAlignment::Center,
+                                VerticalAlignment::SameAsParent
+                            ),
+                            HTML::aligned(HTML::ul(self.skills.iter().map(|(skill, level)| {
+                                if let Some(level) = level {
+                                    HTML::row(vec![HTML::text(skill), HTML::PercentBar(u32::from(*level) * 100 / u32::from(Proficiency::Expert), level.to_string())])
+                                } else {
+                                    HTML::text(skill)
+                                }
+                            }).collect::<Vec<HTML>>()), HorizontalAlignment::Center,
+                            VerticalAlignment::SameAsParent)
+                        ])),
+                        // HTML::section(HTML::aligned(HTML::text("hello world!"), HorizontalAlignment::Center, VerticalAlignment::Center)),
+                    ]),
+                    HTML::section(HTML::col(vec![
+                        HTML::aligned(HTML::fg(HTML::section_title("Education"), Color::Blue), HorizontalAlignment::Center, VerticalAlignment::SameAsParent),
+                        HTML::ol(self.education.iter().map(|e| {
+                            if let Some(degree) = &e.degree {
+                                HTML::text(format!("Attended <b>{}</b> from <i>{}</i> to <i>{}</i> and acheived {}", e.school, e.start_year, e.end_year, degree.to_string()))
+                            } else {
+                                HTML::text(format!("Attended <b>{}</b> from <i>{}</i> to <i>{}</i>", e.school, e.start_year, e.end_year))
+                            }
+                        }).collect::<Vec<HTML>>()),
+                        HTML::aligned(HTML::fg(HTML::section_title("Professional Experience"), Color::Blue), HorizontalAlignment::Center, VerticalAlignment::SameAsParent),
+                        HTML::ul(self.work_experience.iter().map(|j| {
+                            HTML::text(format!("<b>{}</b> at {} from <i>{}</i> to <i>{}</i>", j.position, j.company, j.start_year, j.end_year))
+                        }).collect::<Vec<HTML>>())
+                    ])),
+                ])
+            ])
         ])
     }
 }
